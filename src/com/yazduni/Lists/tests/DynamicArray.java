f58f1f33450ec7e8a00e97797777125ddf968156
Pooria Azami <pooriaazami@gmail.com>
2021-04-03 18:06:03 +0430

    DynamicArray class created

---
+++ src/com/yazduni/Lists/tests/DynamicArray.java
@@ -0,0 +1,47 @@
+package com.yazduni.Lists.tests;
+
+import com.yazduni.Lists.List;
+
+public class DynamicArray<E> implements List<E> {
+
+    private void shrink() {
+    }
+
+    private void grow() {
+    }
+
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public E delete(E data) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int capacity() {
+        return 0;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public void set(E data) {
+
+    }
+}
